c71a06e4e409580fc489ce3e7154d3c004a17c24
002E14744 <Ruc-hi>
2021-10-06 17:11:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'customer-app';
+  customerFirstName: any= '';
+  customerLastName: any = '';
+  customerEmailAddress: any= '';
+  customerSalary: any='';
+
+  AddCustomerDetails: any=[];
+
+
+
+  onAddCustomerData() {
+    // @ts-ignore
+    this.AddCustomerDetails.push({
+      type: 'server',
+      customerFirstName: new this.customerFirstName,
+      customerLastName: new this.customerLastName,
+      customerSalary: new this.customerSalary,
+      customerEmailAddress:new this.customerEmailAddress
+    });
+  }
+  //
+  // onAddCustomerFirstName() {
+  //   // @ts-ignore
+  //   this.onAddCustomerFirstName.name({
+  //   type: 'server',
+  //   name: this.customerFirstName,
+  //   content: this.customerFirstName
+  // });
+  //
+  // }
+  //
+  // onAddCustomerLastName() {
+  //   // @ts-ignore
+  //   this.onAddCustomerLastName.name({
+  //     type: 'server',
+  //     name: this.customerLastName,
+  //     content: this.customerLastName
+  //   });
+  // }
+  //
+  // onAddCustomerEmailAddress() {
+  //   // @ts-ignore
+  //   this.onAddCustomerEmailAddress.name({
+  //     type: 'server',
+  //     name: this.customerEmailAddress,
+  //     content: this.customerEmailAddress
+  //   });
+  // }
+  // onAddCustomerSalary() {
+  //   // @ts-ignore
+  //   this.onAddCustomerSalary.length({
+  //     type: 'server',
+  //     name: this.customerSalary,
+  //     content: this.customerSalary
+  //   });
+  // }
+
+
+
+
+  onAddServer() {
+
+  }
 }
